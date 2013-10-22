382896addec3f86980ff645655c1610fb48cd661
Mary Elizabeth Sullivan <beth.mejsullivan@gmail.com>
2013-10-21 21:42:50 -0400

    add node server.js for http

---
+++ server.js
@@ -0,0 +1,9 @@
+//create http server
+var http = require("http");
+
+http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello World");
+  response.end();
+  //port 8888
+}).listen(8888);
