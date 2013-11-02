4a73b2745f0a374cf996df5445f881eafb42fa1b
Mary Elizabeth Sullivan <beth.mejsullivan@gmail.com>
2013-11-02 17:48:33 -0400

    change server.js into a module that exports the server capabilities

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 //create http server
 var http = require("http");
 
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
-  //port 8888
-}).listen(8888);
+function start() {
+  function onRequest(request, response) {
+    console.log("Request received.");
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello World");
+    response.end();
+  }
+
+  http.createServer(onRequest).listen(8888);
+  console.log("Server has started.");
+}
+
+//exports function
+exports.start = start;
